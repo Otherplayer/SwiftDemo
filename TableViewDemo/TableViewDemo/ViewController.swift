12bd44e10a6c6e46422deb2d68f54379e8f60fda
__无邪_ <lel.guo@haihangyun.com>
2016-10-11 10:09:41 +0800

    add tableView

--- TableViewDemo/TableViewDemo/ViewController.swift
+++ TableViewDemo/TableViewDemo/ViewController.swift
@@ -7,19 +7,98 @@
 //
 
 import UIKit
+import Foundation
 
-class ViewController: UIViewController {
+
+
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate{
+    
+    
+    @IBOutlet weak var editBtn: UIBarButtonItem!
+    let identifier = "identifier"
+    var tableView : UITableView!
+    var datas : NSMutableArray!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.datas = NSMutableArray.init(array: ["a","b","c","d"])
+        
+        let size = UIScreen.main.bounds.size
+        
+        self.tableView = UITableView.init(frame: CGRect(x:0,y:0,width:size.width,height:size.height), style: UITableViewStyle.plain)
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self.view.addSubview(self.tableView)
+        
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: self.identifier)
+        
+        self.tableView.reloadData()
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBAction func editAction(_ sender: UIBarButtonItem) {
+        
+        self.tableView.isEditing = !self.tableView.isEditing
+        
+        if self.tableView.isEditing {
+            self.editBtn.title = "Done"
+        }else{
+            self.editBtn.title = "Edit"
+        }
+        
+    }
+    
+    
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return self.datas.count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let title = self.datas[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: self.identifier, for: indexPath)
+        
+        cell.textLabel?.text = title as? String
+        
+        return cell
+    }
+    
+    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        NSLog("did selected at index : \(indexPath.row) ,result : \(self.datas[indexPath.row])")
+    }
+    
+    public func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    public func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCellEditingStyle {
+        return UITableViewCellEditingStyle.delete
+    }
+    
+    public func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
 
-
+    public func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        self.datas.removeObject(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: UITableViewRowAnimation.automatic)
+    }
+    
+    public func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        tableView.moveRow(at: sourceIndexPath, to: destinationIndexPath)
+        self.datas.exchangeObject(at: sourceIndexPath.row, withObjectAt: destinationIndexPath.row)
+    }
+    
 }
 
